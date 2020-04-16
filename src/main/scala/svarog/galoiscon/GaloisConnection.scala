5335d9ca7553044d64e91f3b1b5bff931e8c46b7
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-04-17 00:12:33 +0200

    add galois connection

--- src/main/scala/svarog/galoiscon/GaloisConnection.scala
+++ src/main/scala/svarog/galoiscon/GaloisConnection.scala
@@ -3,16 +3,24 @@ package svarog.galoiscon
 import svarog.monotone.Monotone
 import svarog.preorders.Preorder
 
-// TODO how we could use GaloisConnection ?
 // TODO how to define left adjoint ?
 // TODO how to define right adjoint ?
-case class GaloisConnection[P,Q](p: Preorder[P], q: Preorder[Q], f: Monotone[P,Q], g: Monotone[Q,P])
+case class GaloisConnection[P,Q](
+  p: Preorder[P],
+  q: Preorder[Q],
+  f: Monotone[P,Q],
+  g: Monotone[Q,P]
+)
 
 trait GaloisConnectionLaw {
+  import svarog.preorders.Preorder.ops._
 
   /** forall p ∈ P, q ∈ Q, f(p) ≤ q iff p ≤ g(q) */
-  def transitivity[P,Q](p: P, q: Q, gc: GaloisConnection[P,Q]): Boolean =
-    if( gc.q.le(gc.f(p), q) ) gc.p.le(p, gc.g(q))
-    else if( gc.p.le(p, gc.g(q)) ) gc.q.le(gc.f(p), q)
-    else false
+  def transitivity[P,Q](p: P, q: Q, gc: GaloisConnection[P,Q]): Boolean = {
+    implicit val pp = gc.p
+    implicit val qq = gc.q
+    if( gc.f(p) <= q ) p <= gc.g(q)
+    else if( p <= gc.g(q) ) gc.f(p) <= q
+    else true
+  }
 }

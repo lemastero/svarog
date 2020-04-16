c31e270e7bb95273a4c68b1386a871bbc351dd85
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-04-17 00:13:44 +0200

    add equivalence simulacrum

--- src/main/scala/svarog/Equivalence.scala
+++ src/main/scala/svarog/Equivalence.scala
@@ -1,30 +1,31 @@
 package svarog
 
+import simulacrum.{op, typeclass}
+
 /**
  * Equivalence relation ~ is a binary operation that is:
  * - (reflexivity) forall a∈A, a ~ a
  * - (symmetry) forall a,b∈A, a ~ b <=> b ~ a
  * - (transitivity) forall a,b,c∈A if a ~ b and b ~ c then a ~ c
  */
+@typeclass
 trait Equivalence[A] {
-  // A ~ B should be a subset of A X B
-  // set (esp. infinite one) can be modeled membership:
-  // member(a) = true if a is from A
-  // is member (a,b) = true if a ~ b
-  def eqivalent(a: A, b: A): Boolean
+  @op("~")
+  def equivalent(a: A, b: A): Boolean
 }
 
 trait EquivalenceLaws {
+  import Equivalence.ops._
 
   def reflexivity[A](a: A)(implicit E: Equivalence[A]): Boolean =
-    E.eqivalent(a,a)
+    a ~ a
 
   def symmetry[A](a: A, b: A)(implicit E: Equivalence[A]): Boolean =
-    if(E.eqivalent(a,b)) E.eqivalent(b,a)
-    else if(E.eqivalent(b,a)) E.eqivalent(a,b)
-    else false
+    if(a ~ b) b ~ a
+    else if(b ~ a) a ~ b
+    else true
 
   def transitivity[A](a: A, b: A, c: A)(implicit E: Equivalence[A]): Boolean =
-    if (E.eqivalent(a,b) && E.eqivalent(b,c)) E.eqivalent(b,c)
-    else false
+    if ((a ~ b) && (b ~ c)) b ~ c
+    else true
 }

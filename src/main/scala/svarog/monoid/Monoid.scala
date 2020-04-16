0e1f9238baf4c67a098a2bb7b11647750f771986
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-04-17 00:12:47 +0200

    add monoid typeclass

--- src/main/scala/svarog/monoid/Monoid.scala
+++ src/main/scala/svarog/monoid/Monoid.scala
@@ -1,7 +1,12 @@
 package svarog.monoid
 
+import simulacrum.{op, typeclass}
+
+@typeclass
 trait Monoid[X] {
-  def unit: X
+  def I: X
+
+  @op("*")
   def multiply(a: X, b: X): X
 }
 
@@ -9,19 +14,18 @@ trait MonoidLaws {
 
   /** forall a ∈ X, I ⊗ a􏰆 = a and a ⊗ I = 􏰆a */
   def unitality[X](a: X)(implicit P: Monoid[X]): Boolean = {
-    import P._
-    val ua = multiply(unit, a)
-    val au = multiply(a, unit)
-    (ua == a) && (a == au)
+    import Monoid.ops._
+    import P.I
+    ((I * a) == a) && (a == (a * I))
   }
 
   /** forall a,b,c ∈ X, (a ⊗ b) ⊗ c􏰆 = a ⊗ (b ⊗ c) */
   def associativity[X](a: X, b: X, c: X)(implicit P: Monoid[X]): Boolean = {
-    import P._
-    val ab = multiply(a, b)
-    val bc = multiply(b, c)
-    multiply(ab,c) == multiply(a, bc)
+    import Monoid.ops._
+    ((a * b) * c) == (a * (b * c))
   }
 }
+
+object MonoidLaws extends MonoidLaws {}
 
 

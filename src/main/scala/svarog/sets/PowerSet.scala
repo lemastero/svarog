1502fe695b34f147d22c4d874c167c86d94dee61
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-04-19 12:55:29 +0200

    add power set

---
+++ src/main/scala/svarog/sets/PowerSet.scala
@@ -0,0 +1,19 @@
+package svarog.sets
+
+class PowerSet[X](all: Set[Set[X]]) extends MathSet[Set[X]] {
+  override def apply(x: Set[X]): Boolean =
+    all.contains(x)
+}
+
+object PowerSet {
+
+  // set of all susets
+//  class PowersetOfType[X] {
+//    def apply[Y]: Boolean =
+//      classOf[Y].isAssignableFrom(classOf[X])
+//  }
+
+  class SetOfPowerSet[X](x: MathSet[X]) {
+    def apply[Y <: X](y: Y): Boolean = x(y)
+  }
+}
